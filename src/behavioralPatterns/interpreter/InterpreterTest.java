ff9f460a1e90ce82c73b0e3304e6949deb87ada3
Taras Tarasyuk <worktarasyuk@gmail.com>
2021-10-16 16:39:04 +0300

    add interpreter pattern

--- src/behavioralPatterns/interpreter/InterpreterTest.java
+++ src/behavioralPatterns/interpreter/InterpreterTest.java
@@ -13,8 +13,9 @@ package behavioralPatterns.interpreter;
 public class InterpreterTest {
     public static void main(String[] args) {
         // 1 - 2 + 3
-
-
+        Context context = new Context();
+        Expression expression = context.evaluate("-1+2+3");
+        System.out.println(expression.interpret());
     }
 }
 
@@ -76,7 +77,7 @@ class Context {
                 pos--;
             } else {
                 Expression left = evaluate(s.substring(0, pos));
-                Expression right = new NumberExpression(Integer.parseInt(s.substring(pos+1, s.length())));
+                Expression right = new NumberExpression(Integer.parseInt(s.substring(pos+1)));
                 char operator = s.charAt(pos);
                 switch (operator) {
                     case '-':
