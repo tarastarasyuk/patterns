d0234aa385ab190f19a62619303887823c7edf58
Taras Tarasyuk <worktarasyuk@gmail.com>
2021-10-16 16:35:31 +0300

    add interpreter pattern

---
+++ src/behavioralPatterns/interpreter/InterpreterTest.java
@@ -0,0 +1,93 @@
+package behavioralPatterns.interpreter;
+
+/**
+ * Ціль: визначення представлення граматики об'єкта
+ *
+ * Для чого використовується: використовується для визначення граматики заданої мови и інтерпритації її речень
+ *
+ * Приклад використання:
+ *
+ * -спрощення ієрархії класів за допомогою інтерпритації
+ */
+
+public class InterpreterTest {
+    public static void main(String[] args) {
+        // 1 - 2 + 3
+
+
+    }
+}
+
+interface Expression {
+    int interpret();
+}
+
+class NumberExpression implements Expression {
+    int number;
+
+    public NumberExpression(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public int interpret() {
+        return number;
+    }
+}
+
+class MinusExpression implements Expression {
+    Expression left;
+    Expression right;
+
+    public MinusExpression(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int interpret() {
+        return left.interpret() - right.interpret();
+    }
+}
+
+class PlusExpression implements Expression {
+    Expression left;
+    Expression right;
+
+    public PlusExpression(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int interpret() {
+        return left.interpret() + right.interpret();
+    }
+}
+
+/**
+ * Виконання всієї логіки
+ */
+class Context {
+    Expression evaluate(String s) {
+        int pos = s.length() - 1;
+        while (pos < 0) {
+            if (Character.isDigit(s.charAt(pos))) {
+                pos--;
+            } else {
+                Expression left = evaluate(s.substring(0, pos));
+                Expression right = new NumberExpression(Integer.parseInt(s.substring(pos+1, s.length())));
+                char operator = s.charAt(pos);
+                switch (operator) {
+                    case '-':
+                        return new MinusExpression(left, right);
+                    case '+':
+                        return new PlusExpression(left, right);
+                }
+            }
+        }
+        int result = Integer.parseInt(s);
+
+        return new NumberExpression(result);
+    }
+}
